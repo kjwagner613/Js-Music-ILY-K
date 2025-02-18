708de36dfea087f94d1a16966133567259d69ba0
kev613 <kjwagner613@gmail.com>
2025-02-17 18:38:23 -0800

    funcional not complete

--- script.js
+++ script.js
@@ -18,6 +18,66 @@ document.addEventListener("DOMContentLoaded", () => {
       name: "PNAU Mix",
       file: "https://dl.dropboxusercontent.com/scl/fi/4p9hpo906qide0dro6r8x/03-Cold-Heart-PNAU-Remix.mp3?rlkey=hqnjviz5cq2hlkxmy1gzml67w&st=tlvwqce6&dl=0",
     },
+    {
+      name: "Death Wish Love",
+      file: "https://dl.dropboxusercontent.com/scl/fi/sqt4ep4drkmb2p6n427ri/Death-Wish-Love.mp3?rlkey=y350qtwje8y4mrpublj3upl97&st=049g959w&dl=0",
+    },
+    {
+      name: "I Am The Highway",
+      file: "https://dl.dropboxusercontent.com/scl/fi/qd9fefsq6txiwnkw97gzw/I-Am-The-Highway.mp3?rlkey=y4kh6hssi7xnpk414i80ero9d&st=uuw8nthw&dl=0",
+    },
+    {
+      name: "I Get High",
+      file: "https://dl.dropboxusercontent.com/scl/fi/m0nsuopkro244w30on1ej/I-Get-High.mp3?rlkey=giuzvlv8i1euu2qb3iix5xe6p&st=wbe5y86r&dl=0",
+    },
+    {
+      name: "Prettty Slowly",
+      file: "https://dl.dropboxusercontent.com/scl/fi/ui9dc8r8fxpm5s6tx8ymi/Pretty-Slowly.mp3?rlkey=k1mib3p97kr51q5jhul1kifda&st=7ho939vn&dl=0",
+    },
+    {
+      name: "Relax My Beloved",
+      file: "https://dl.dropboxusercontent.com/scl/fi/6pyst69xaqp9hmiiv2axz/Relax-My-Beloved.mp3?rlkey=qgkkvxorfnhdh3hhd9ktffe4w&st=o68k14qd&dl=0",
+    },
+    {
+      name: "The Craving",
+      file: "https://dl.dropboxusercontent.com/scl/fi/u2io5qs34uydyev225wz0/The-Craving-Jenna-s-version.mp3?rlkey=kqqvt6b0usswdjfsucwy0aj39&st=z4msabkj&dl=0",
+    },
+    {
+      name: "Too Close",
+      file: "https://dl.dropboxusercontent.com/scl/fi/agebvadhjk35vkf6kk0m7/Too-Close.mp3?rlkey=7j1d8jug58ak57js5ikn1k11s&st=3mrugfu8&dl=0",
+    },
+    {
+      name: "Volcano",
+      file: "https://dl.dropboxusercontent.com/scl/fi/8859rfgqzp1m5gjx3g81i/Volcano.mp3?rlkey=aibl7xc3hxurvq4luhc1ciftt&st=1718ns5x&dl=0",
+    },
+    {
+      name: "Wilder Mind",
+      file: "https://dl.dropboxusercontent.com/scl/fi/k267tm5yrh0vigh8yg36q/Wilder-Mind.mp3?rlkey=rxvsk0ryu8l3a1vyekjiilo8z&st=hz0s1pzx&dl=0",
+    },
+    {
+      name: "You and Me",
+      file: "https://dl.dropboxusercontent.com/scl/fi/str3lbg8enqezfzahzmz8/You-and-Me.mp3?rlkey=dfqwaaswjobips35chr85j8r5&st=en4xe588&dl=0",
+    },
+    {
+      name: "Angel By The Wings",
+      file: "https://dl.dropboxusercontent.com/scl/fi/o54urjpf9rfds2rjdkg22/Angel-By-The-Wings.mp3?rlkey=mup2w5wpdcp7hfx3570ec9i3f&st=wos2bjib&dl=0",
+    },
+    {
+      name: "Anybody Listening",
+      file: "https://dl.dropboxusercontent.com/scl/fi/td17kqv7x6zhdn5pg6lq7/Anybody-Listening_.mp3?rlkey=c1di04qsfyb9tauowpza7i2xu&st=xwqrdgrs&dl=0",
+    },
+    {
+      name: "Bad Child",
+      file: "https://dl.dropboxusercontent.com/scl/fi/57gty80uysuziwjq1wtg3/Bad-Child.mp3?rlkey=89l1r02m3q04eq5282fgenr6z&st=p4lngp62&dl=0",
+    },
+    {
+      name: "Beautiful Things",
+      file: "https://dl.dropboxusercontent.com/scl/fi/h3jq6f1yk5tdo5jxy6go5/Beautiful-Things.mp3?rlkey=avgln5y50ykm2m39azbvxdord&st=t0b5p0cz&dl=0",
+    },
+    {
+      name: "Natasha Bedingfield",
+      file: "https://dl.dropboxusercontent.com/scl/fi/z0qjlydj4x2t3c5qaggxe/Unwritten.mp3?rlkey=0j5dpxnkq0ra0kgpgeiomyewf&st=t1tg6lft&dl=0",
+    },
   ];
 
   let currentSongIndex = 0; // Initialize current song index
@@ -33,8 +93,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function playSong(index) {
     audioPlayer.src = songs[index].file;
     audioPlayer.play().catch((error) => {
-      console.error("Error playing song:", error); // Handle potential errors
-      // You might want to display an error message to the user here.
+      console.error("Error playing song:", error); 
     });
     currentSongIndex = index;
   }
